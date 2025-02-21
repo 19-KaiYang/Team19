e979eb9bcec1b01e8914ace26ade35278f892c85
13-Clef <114891810+13-Clef@users.noreply.github.com>
2025-02-21 14:10:13 +0800

    updated scriptable object and display ingredients for selected item

--- Team19_OxygenZero/Assets/Clef_Scripts/CraftingScript.cs
+++ Team19_OxygenZero/Assets/Clef_Scripts/CraftingScript.cs
@@ -1,10 +1,64 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CraftingScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<CraftingRecipe> recipes; // list of all available recipes
+
+    public TextMeshProUGUI selectedItemText; // UI text to display selected item name
+    public TextMeshProUGUI ingredient1Text; // first ingredient
+    public TextMeshProUGUI ingredient2Text; // second ingredient
+
+    private CraftingRecipe selectedRecipe; // the currently selected recipe
+
+    public void SelectItem(string itemName)
+    {
+        // find the recipe for the selected item
+        selectedRecipe = recipes.Find( r => r.result.itemName == itemName);
+
+        if (selectedRecipe != null)
+        {
+            // update the selected item name
+            selectedItemText.text = "<b>" + selectedRecipe.result.itemName + "</b>";
+
+            // set ingredient texts
+            if (selectedRecipe.ingredients.Length > 0)
+            {
+                ingredient1Text.text = selectedRecipe.ingredients[0].item.itemName + " x" + selectedRecipe.ingredients[0].amount;
+            }
+            else
+            {
+                ingredient1Text.text = "";
+            }
+            
+            if (selectedRecipe.ingredients.Length > 1)
+            {
+                ingredient2Text.text = selectedRecipe.ingredients[1].item.itemName + " x" + selectedRecipe.ingredients[1].amount;
+            }
+            else
+            {
+                ingredient2Text.text = "";
+            }
+            
+        }
+        else
+        {
+            Debug.LogError("No recipe found for: " + itemName);
+        }
+    }
+
+    public void onClickOxygenTank()
+    {
+        SelectItem("Oxygen Tank");
+    }
+
+    public void OnClickBatteryPack()
+    {
+        SelectItem("Battery Pack");
+    }
+
+    // Start is called beforre the first frame update
     void Start()
     {
         
